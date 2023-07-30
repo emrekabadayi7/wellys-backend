d09bc8b9352b0460029df950abb57e02e04926b1
emrekabadayi7 <emrekabadayi7@gmail.com>
2023-07-30 17:50:16 -0500

    bodyparserli commit

--- api.js
+++ api.js
@@ -1,9 +1,22 @@
 const express = require("express");
 const cors = require("cors")
+const bodyParser = require("body-parser");
 const dboperations = require("./dboperations");
 
 const app = express();
 app.use(cors());
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type");
+  next();
+});
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  next();
+});
 
 dboperations.getOrders().then(result => {
     console.log(result);
@@ -24,5 +37,7 @@ const PORT = 4000;
 app.listen(PORT, () => {
   console.log(`Server listening on port ${PORT}`);
 });
+
+
 
 module.exports = app
